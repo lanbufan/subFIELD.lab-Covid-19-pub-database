ec110f40ca95676f7e54d2f3c1a5328182ed7180
awakenedhaki <rodrigo.vallejosch@gmail.com>
2020-04-14 15:04:28 -0700

    Move to pipeline/

---
+++ pipeline/main.py
@@ -0,0 +1,15 @@
+# DEPENDENCIES
+from crossrefapiclient import Works
+from litcovid import PubTator, update_litcovid_tsv
+
+# CONSTANTS
+PUBTATOR_TYPE = 'biocjson'
+
+# OBJECTS
+works = Works()
+pubtator = PubTator(PUBTATOR_TYPE)
+
+if __name__ == '__main__':
+    # Update LitCovid TSV file
+    # . Provides recently enter PMIDs
+    pmids = update_litcovid_tsv()
